ee653a46cc458128e15bbd23554e705d4e993525
jason hwang <cd4761@gmail.com>
2025-08-18 16:10:53 +0900

    Use official GitHub Pages Next.js workflow

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  basePath: process.env.NODE_ENV === 'production' ? '/staking-community-version' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/staking-community-version/' : '',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
